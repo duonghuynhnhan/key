f56051fbe1de06929a8e4035f993f206b95e2c9b
Kevin Duong (Dương Huỳnh Nhân) <74961393+duonghuynhnhan@users.noreply.github.com>
2023-04-24 21:44:47 +0700

    Add files via upload

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //const url = "mongodb://localhost:27017/EmployeeDB";
 //const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
-const url = "mongodb+srv://cluster0.uoqoa4t.mongodb.net/myFirstDatabase"
+const url = "mongodb+srv://minhb1900349:minh@cluster0.uoqoa4t.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(url, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log("MongoDB Connection Succeeded"); }
     else {
