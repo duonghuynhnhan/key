430ac9b685c3773da7bd46ade389dcc5099c476b
Kevin Duong (Dương Huỳnh Nhân) <74961393+duonghuynhnhan@users.noreply.github.com>
2023-04-27 13:36:34 +0700

    Update db.js

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //const url = "mongodb://localhost:27017/EmployeeDB";
 //const url = "mongodb+srv://tmtuan:**************@cluster0.zfovn.mongodb.net/test"
-const url = "mongodb+srv://minhb1900349:minh@cluster0.uoqoa4t.mongodb.net/?retryWrites=true&w=majority"
+const url = "mongodb+srv://nhanb1910676:kevin@cluster0.bqdg02m.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(url, { useNewUrlParser: true }, (err) => {
     if (!err) { console.log("MongoDB Connection Succeeded"); }
     else {
